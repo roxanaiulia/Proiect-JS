19b10cd64c661f31181df308eca763155912e994
Roxana Iulia <roxanaiuliab@gmail.com>
2023-03-23 01:08:38 +0200

    Update

---
+++ form.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("the-form");
+
+form.addEventListener ("submit", (event) => {
+
+    event.preventDefault();
+
+});
---
+++ script.js
@@ -0,0 +1,16 @@
+
+function toggleMenu() {
+  var x = document.getElementById("hamburgerMenu");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+
+
+
+
+
+
+//Nu am apucat sa fac si al 3-lea event.
